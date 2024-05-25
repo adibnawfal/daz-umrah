8dec96fa9fa2e787ad700de548f5d3b3269e2c08
Adib Nawfal <56248371+adibnawfal@users.noreply.github.com>
2024-05-25 20:24:10 +0800

    📝 Update module

--- resources/views/welcome/view.blade.php
+++ resources/views/welcome/view.blade.php
@@ -4,7 +4,7 @@
   <main id="scrollspy-1" class="flex flex-col flex-grow dark:bg-gray-800">
     <!-- Home -->
     <div
-      class="overflow-hidden bg-center bg-[url('https://images.unsplash.com/photo-1633174754673-95dd2842f8fc?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+      class="overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1633174754673-95dd2842f8fc?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
       <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-20">
         @include('welcome.partials.home')
       </div>
